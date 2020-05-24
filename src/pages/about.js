11df9d246e3527759a3e43b92b72227ac64ed19e
Valendea Amalia <valendea.amalia@gmail.com>
2020-05-24 15:17:16 +0700

    rerun npm

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,7 +8,7 @@ const About = () => (
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea dignissimos
       aut consequuntur aspernatur corrupti ratione, odit similique tenetur
       accusantium, est nostrum esse minus iure voluptatum nihil cumque
-      blanditiis non? Odit.
+      blanditiis non? Odit. 
     </p>
   </Layout>
 );
